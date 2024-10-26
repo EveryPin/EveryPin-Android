bc199e521b88ea3879a996191871099f4c6a8b77
Hong Sunghwi <tnfy10@gmail.com>
2024-10-26 10:53:24 +0900

    targetSdk, compileSdk 버전 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,12 +15,12 @@ plugins {
 
 android {
     namespace = "everypin.app"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "everypin.app"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
